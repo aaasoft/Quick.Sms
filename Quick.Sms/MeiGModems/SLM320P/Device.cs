16fe809b07dae27b4055970e1a2d25f90f814b66
张鹏 <scbeta@qq.com>
2022-06-06 16:12:50 +0800

    修复一个命名空间错误。

--- Quick.Sms/MeiGModems/SLM320P/Device.cs
+++ Quick.Sms/MeiGModems/SLM320P/Device.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-namespace Quick.Sms.Device.SLM320P
+namespace Quick.Sms.MeiGModems.SLM320P
 {
     public class Device : AbstractSerialPortModem
     {
