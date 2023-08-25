06fe0732b0996ffec0100dcf0d3fee870ef7691c
张鹏 <scbeta@qq.com>
2023-08-25 16:15:47 +0800

    完成全部功能移植

--- src/Quick.Sms.Avalonia/MainWindow.axaml.cs
+++ src/Quick.Sms.Avalonia/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.VisualTree;
 using System.Threading.Tasks;
 
 namespace Quick.Sms.Avalonia;
 
 public partial class MainWindow : Window
 {
+    private ScrollViewer txtLogsScrollViewer;
     public MainWindow()
     {
         var viewModel = new ViewModels.MainWindowViewModel();
@@ -13,10 +16,26 @@ public partial class MainWindow : Window
         Title = viewModel.Title;
         InitializeComponent();
         txtLogs.TextChanged += TxtLogs_TextChanged;
+        txtLogsScrollViewer = FindVisualTreeChild<ScrollViewer>(txtLogs);
+    }
+
+    private T FindVisualTreeChild<T>(Visual visual) where T : Visual
+    {
+        foreach (var child in visual.GetVisualChildren())
+        {
+            if (visual is T t)
+                return t;
+            var childT = FindVisualTreeChild<T>(child);
+            if (childT != null)
+                return childT;
+        }
+        return null;
     }
 
     private void TxtLogs_TextChanged(object sender, TextChangedEventArgs e)
     {
-        txtLogs.CaretIndex = int.MaxValue;
+        if (txtLogsScrollViewer == null)
+            txtLogsScrollViewer = FindVisualTreeChild<ScrollViewer>(txtLogs);
+        txtLogsScrollViewer?.ScrollToEnd();
     }
 }
